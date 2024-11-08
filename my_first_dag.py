54a372a159c9934eb4f12be5b9c9fc0dea95f3fb
«Sergey» <notan1979@yandex.ru>
2024-11-08 23:22:38 +0300

    1

---
+++ my_first_dag.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+def print_hello():
+    return 'Привет Мир, первый запуск Airflow DAG!'
+
+dag = DAG('my_first_dag', description='Доброе пожаловать Илья, с вами DAG',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
+
+hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+
+hello_operator
